2555523ae5f7f2b756e0a850175c1fbadf6332bf
b0b013 <97jeepwranglerforsale@gmail.com>
2017-04-02 16:21:04 -0700

    added some comments. added some math at the end

--- l3.py
+++ l3.py
@@ -9,6 +9,7 @@ print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
 print "Is it true that 3 + 2 < 5 - 7?"
 
+# solves each side separately before comparison
 print 3 + 2 < 5 - 7
 
 print "What is 3 + 2?", 3 + 2
@@ -18,6 +19,11 @@ print "Oh, that's why it's False."
 
 print "How about some more."
 
+# prints True or False based on comparison at the end of the statemnt
 print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2
 print "Is it less or equal?", 5 <= -2
+
+print 100 - 25 * 3
+print 3 % 4
+print 25 * 3 % 4
